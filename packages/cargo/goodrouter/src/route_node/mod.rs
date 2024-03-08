83c109d891f1bb0eec893ae59a8c100c57f87c7d
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-03-08 14:12:27 +0100

    formatting

--- packages/cargo/goodrouter/src/route_node/mod.rs
+++ packages/cargo/goodrouter/src/route_node/mod.rs
@@ -4,10 +4,10 @@ pub mod route_node_utility;
 
 use route_node_utility::*;
 use std::{
-    cell::RefCell,
-    cmp::Ordering,
-    collections::BTreeSet,
-    rc::{Rc, Weak},
+  cell::RefCell,
+  cmp::Ordering,
+  collections::BTreeSet,
+  rc::{Rc, Weak},
 };
 
 pub type RouteNodeRc<'r, K> = Rc<RefCell<RouteNode<'r, K>>>;
@@ -15,112 +15,112 @@ pub type RouteNodeWeak<'r, K> = Weak<RefCell<RouteNode<'r, K>>>;
 
 #[derive(Debug)]
 pub struct RouteNode<'r, K> {
-    // the route's key, if any
-    pub route_key: Option<K>,
-    // the route parameter names
-    pub route_parameter_names: Vec<&'r str>,
-    // suffix that comes after the parameter value (if any!) of the path
-    anchor: &'r str,
-    // does this node has a parameter
-    has_parameter: bool,
-    // children that represent the rest of the path that needs to be matched
-    children: BTreeSet<RouteNodeRc<'r, K>>,
-    // parent node, should only be null for the root node
-    parent: Option<RouteNodeWeak<'r, K>>,
+  // the route's key, if any
+  pub route_key: Option<K>,
+  // the route parameter names
+  pub route_parameter_names: Vec<&'r str>,
+  // suffix that comes after the parameter value (if any!) of the path
+  anchor: &'r str,
+  // does this node has a parameter
+  has_parameter: bool,
+  // children that represent the rest of the path that needs to be matched
+  children: BTreeSet<RouteNodeRc<'r, K>>,
+  // parent node, should only be null for the root node
+  parent: Option<RouteNodeWeak<'r, K>>,
 }
 
 impl<'r, K> Ord for RouteNode<'r, K> {
-    fn cmp(&self, other: &Self) -> Ordering {
-        if self.anchor.len() < other.anchor.len() {
-            return Ordering::Greater;
-        }
-        if self.anchor.len() > other.anchor.len() {
-            return Ordering::Less;
-        }
-
-        if !self.has_parameter && other.has_parameter {
-            return Ordering::Less;
-        }
-        if self.has_parameter && !other.has_parameter {
-            return Ordering::Greater;
-        }
-
-        if self.anchor < other.anchor {
-            return Ordering::Less;
-        }
-        if self.anchor > other.anchor {
-            return Ordering::Greater;
-        }
-
-        Ordering::Equal
+  fn cmp(&self, other: &Self) -> Ordering {
+    if self.anchor.len() < other.anchor.len() {
+      return Ordering::Greater;
     }
+    if self.anchor.len() > other.anchor.len() {
+      return Ordering::Less;
+    }
+
+    if !self.has_parameter && other.has_parameter {
+      return Ordering::Less;
+    }
+    if self.has_parameter && !other.has_parameter {
+      return Ordering::Greater;
+    }
+
+    if self.anchor < other.anchor {
+      return Ordering::Less;
+    }
+    if self.anchor > other.anchor {
+      return Ordering::Greater;
+    }
+
+    Ordering::Equal
+  }
 }
 
 impl<'r, K> PartialOrd for RouteNode<'r, K> {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
+  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+    Some(self.cmp(other))
+  }
 }
 
 impl<'r, K> Eq for RouteNode<'r, K> {}
 
 impl<'r, K> PartialEq for RouteNode<'r, K> {
-    fn eq(&self, other: &Self) -> bool {
-        self.anchor == other.anchor && self.has_parameter == other.has_parameter
-    }
+  fn eq(&self, other: &Self) -> bool {
+    self.anchor == other.anchor && self.has_parameter == other.has_parameter
+  }
 }
 
 impl<'r, K> Default for RouteNode<'r, K> {
-    fn default() -> Self {
-        Self {
-            route_key: None,
-            route_parameter_names: Default::default(),
-            anchor: Default::default(),
-            has_parameter: Default::default(),
-            children: Default::default(),
-            parent: Default::default(),
-        }
+  fn default() -> Self {
+    Self {
+      route_key: None,
+      route_parameter_names: Default::default(),
+      anchor: Default::default(),
+      has_parameter: Default::default(),
+      children: Default::default(),
+      parent: Default::default(),
     }
+  }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use itertools::Itertools;
-    use std::iter::FromIterator;
+  use super::*;
+  use itertools::Itertools;
+  use std::iter::FromIterator;
 
-    #[test]
-    fn route_ordering() {
-        let nodes = vec![
-            RouteNode {
-                route_key: None,
-                has_parameter: false,
-                anchor: "aa",
-                ..Default::default()
-            },
-            RouteNode {
-                route_key: Some(&1),
-                has_parameter: false,
-                anchor: "xx",
-                ..Default::default()
-            },
-            RouteNode {
-                route_key: None,
-                has_parameter: true,
-                anchor: "aa",
-                ..Default::default()
-            },
-            RouteNode {
-                route_key: None,
-                has_parameter: false,
-                anchor: "x",
-                ..Default::default()
-            },
-        ];
+  #[test]
+  fn route_ordering() {
+    let nodes = vec![
+      RouteNode {
+        route_key: None,
+        has_parameter: false,
+        anchor: "aa",
+        ..Default::default()
+      },
+      RouteNode {
+        route_key: Some(&1),
+        has_parameter: false,
+        anchor: "xx",
+        ..Default::default()
+      },
+      RouteNode {
+        route_key: None,
+        has_parameter: true,
+        anchor: "aa",
+        ..Default::default()
+      },
+      RouteNode {
+        route_key: None,
+        has_parameter: false,
+        anchor: "x",
+        ..Default::default()
+      },
+    ];
 
-        let nodes_expected = nodes.iter();
-        let nodes_actual = nodes.iter().sorted();
+    let nodes_expected = nodes.iter();
+    let nodes_actual = nodes.iter().sorted();
 
-        assert_eq!(Vec::from_iter(nodes_actual), Vec::from_iter(nodes_expected));
-    }
+    assert_eq!(Vec::from_iter(nodes_actual), Vec::from_iter(nodes_expected));
+  }
 }
--- packages/cargo/goodrouter/src/template/template_pairs.rs
+++ packages/cargo/goodrouter/src/template/template_pairs.rs
@@ -2,83 +2,81 @@ use super::template_parts::{parse_template_parts, TemplateParts};
 use regex::Regex;
 
 pub fn parse_template_pairs<'r>(template: &'r str, re: &'r Regex) -> TemplatePairs<'r> {
-    TemplatePairs::new(template, re)
+  TemplatePairs::new(template, re)
 }
 
 pub struct TemplatePairs<'r> {
-    parts: TemplateParts<'r>,
-    index: usize,
-    is_finished: bool,
+  parts: TemplateParts<'r>,
+  index: usize,
+  is_finished: bool,
 }
 
 impl<'r> TemplatePairs<'r> {
-    fn new(template: &'r str, re: &'r Regex) -> Self {
-        let parts = parse_template_parts(template, re);
-        let index = 0;
-        let is_finished = false;
+  fn new(template: &'r str, re: &'r Regex) -> Self {
+    let parts = parse_template_parts(template, re);
+    let index = 0;
+    let is_finished = false;
 
-        Self {
-            parts,
-            index,
-            is_finished,
-        }
+    Self {
+      parts,
+      index,
+      is_finished,
     }
+  }
 }
 
 impl<'r> Iterator for TemplatePairs<'r> {
-    type Item = (&'r str, Option<&'r str>);
+  type Item = (&'r str, Option<&'r str>);
 
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.is_finished {
-            return None;
-        }
-
-        let result = if let Some(part) = self.parts.next() {
-            if self.index == 0 {
-                Some((part, None))
-            } else {
-                let anchor = self.parts.next().unwrap();
-                Some((anchor, Some(part)))
-            }
-        } else {
-            None
-        };
-
-        self.index += 1;
-
-        if result.is_none() {
-            self.is_finished = true;
-        }
-
-        result
+  fn next(&mut self) -> Option<Self::Item> {
+    if self.is_finished {
+      return None;
     }
+
+    let result = if let Some(part) = self.parts.next() {
+      if self.index == 0 {
+        Some((part, None))
+      } else {
+        let anchor = self.parts.next().unwrap();
+        Some((anchor, Some(part)))
+      }
+    } else {
+      None
+    };
+
+    self.index += 1;
+
+    if result.is_none() {
+      self.is_finished = true;
+    }
+
+    result
+  }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::super::TEMPLATE_PLACEHOLDER_REGEX;
-    use super::*;
+  use super::super::TEMPLATE_PLACEHOLDER_REGEX;
+  use super::*;
 
-    #[test]
-    fn parse_template_pairs_test() {
-        let pairs: Vec<_> =
-            parse_template_pairs("/a/{b}/{c}", &TEMPLATE_PLACEHOLDER_REGEX).collect();
+  #[test]
+  fn parse_template_pairs_test() {
+    let pairs: Vec<_> = parse_template_pairs("/a/{b}/{c}", &TEMPLATE_PLACEHOLDER_REGEX).collect();
 
-        assert_eq!(
-            pairs,
-            vec![("/a/", None), ("/", Some("b")), ("", Some("c"))]
-        );
+    assert_eq!(
+      pairs,
+      vec![("/a/", None), ("/", Some("b")), ("", Some("c"))]
+    );
 
-        let pairs: Vec<_> =
-            parse_template_pairs("/a/{b}/{c}/", &TEMPLATE_PLACEHOLDER_REGEX).collect();
+    let pairs: Vec<_> = parse_template_pairs("/a/{b}/{c}/", &TEMPLATE_PLACEHOLDER_REGEX).collect();
 
-        assert_eq!(
-            pairs,
-            vec![("/a/", None), ("/", Some("b")), ("/", Some("c"))]
-        );
+    assert_eq!(
+      pairs,
+      vec![("/a/", None), ("/", Some("b")), ("/", Some("c"))]
+    );
 
-        let pairs: Vec<_> = parse_template_pairs("", &TEMPLATE_PLACEHOLDER_REGEX).collect();
+    let pairs: Vec<_> = parse_template_pairs("", &TEMPLATE_PLACEHOLDER_REGEX).collect();
 
-        assert_eq!(pairs, vec![("", None)])
-    }
+    assert_eq!(pairs, vec![("", None)])
+  }
 }
