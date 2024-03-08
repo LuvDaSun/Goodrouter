01e33764703116e3847480a68699193d44283f65
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-03-08 21:27:04 +0100

    format

--- packages/net/Goodrouter.Bench/Program.cs
+++ packages/net/Goodrouter.Bench/Program.cs
@@ -2,9 +2,9 @@ using BenchmarkDotNet.Running;
 
 public class Program
 {
-    public static void Main(string[] args)
-    {
-        BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
-    }
+  public static void Main(string[] args)
+  {
+    BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+  }
 }
 
--- packages/net/Goodrouter.Bench/RouterBench.cs
+++ packages/net/Goodrouter.Bench/RouterBench.cs
@@ -4,88 +4,88 @@ using BenchmarkDotNet.Attributes;
 
 public class RouterBenchSmall : RouterBenchBase
 {
-    public RouterBenchSmall() : base("small")
-    {
-    }
+  public RouterBenchSmall() : base("small")
+  {
+  }
 }
 
 public class RouterBenchDocker : RouterBenchBase
 {
-    public RouterBenchDocker() : base("docker")
-    {
-    }
+  public RouterBenchDocker() : base("docker")
+  {
+  }
 }
 
 public class RouterBenchGithub : RouterBenchBase
 {
-    public RouterBenchGithub() : base("github")
-    {
-    }
+  public RouterBenchGithub() : base("github")
+  {
+  }
 }
 
 public abstract class RouterBenchBase
 {
 
-    protected RouterBenchBase(string name)
+  protected RouterBenchBase(string name)
+  {
+    templates = System.IO.File.ReadLines("" + name + ".txt").
+       Where(line => line.Length > 0).
+       ToArray();
+
+    var allParameterNames = templates.
+        SelectMany(
+            template => TemplateUtility.ParseTemplateParts(template, parameterPlaceholderRE).
+                Where((part, index) => index % 2 != 0)
+        ).
+        ToHashSet();
+
+    allParameters = allParameterNames.
+       Select((name, index) => (name, "p" + index)).
+       ToDictionary(pair => pair.name, pair => pair.Item2);
+
+    templateCount = templates.Count;
+
+    router = new Router<string>();
+    foreach (var template in templates)
     {
-        templates = System.IO.File.ReadLines("" + name + ".txt").
-           Where(line => line.Length > 0).
-           ToArray();
-
-        var allParameterNames = templates.
-            SelectMany(
-                template => TemplateUtility.ParseTemplateParts(template, parameterPlaceholderRE).
-                    Where((part, index) => index % 2 != 0)
-            ).
-            ToHashSet();
-
-        allParameters = allParameterNames.
-           Select((name, index) => (name, "p" + index)).
-           ToDictionary(pair => pair.name, pair => pair.Item2);
-
-        templateCount = templates.Count;
-
-        router = new Router<string>();
-        foreach (var template in templates)
-        {
-            router.InsertRoute(template, template);
-        }
-
-        paths = templates.
-           Select(template =>
-               {
-                   var path = router.StringifyRoute(template, allParameters);
-                   return path;
-               }
-           ).
-           ToArray() as string[];
+      router.InsertRoute(template, template);
     }
 
-    private Router<string> router;
-    private Regex parameterPlaceholderRE = new Regex("\\{(.*?)\\}");
-    private int index = 0;
-    private readonly IList<string> paths;
-    private readonly IList<string> templates;
-    private readonly int templateCount;
-    private readonly IReadOnlyDictionary<string, string> allParameters;
+    paths = templates.
+       Select(template =>
+           {
+             var path = router.StringifyRoute(template, allParameters);
+             return path;
+           }
+       ).
+       ToArray() as string[];
+  }
 
-    [Benchmark]
-    public void RouterParseBench()
-    {
-        var path = paths[index % templateCount];
-        router.ParseRoute(path);
+  private Router<string> router;
+  private Regex parameterPlaceholderRE = new Regex("\\{(.*?)\\}");
+  private int index = 0;
+  private readonly IList<string> paths;
+  private readonly IList<string> templates;
+  private readonly int templateCount;
+  private readonly IReadOnlyDictionary<string, string> allParameters;
 
-        index++;
-    }
+  [Benchmark]
+  public void RouterParseBench()
+  {
+    var path = paths[index % templateCount];
+    router.ParseRoute(path);
 
-    [Benchmark]
-    public void RouterStringifyBench()
-    {
-        var template = templates[index % templateCount];
-        router.StringifyRoute(template, allParameters);
+    index++;
+  }
 
-        index++;
-    }
+  [Benchmark]
+  public void RouterStringifyBench()
+  {
+    var template = templates[index % templateCount];
+    router.StringifyRoute(template, allParameters);
+
+    index++;
+  }
 
 }
 
